7bb10c1493338823d609f79bf1a30c00a4efe585
Evgeniy Beskrovnyy <e.beskrovnyy@gmail.com>
2016-06-18 17:33:14 +0400

    first

---
+++ js/common.js
@@ -0,0 +1,51 @@
+
+$(function() {
+
+	document.getElementById('svg-icons').innerHTML = SVG_ICONS;
+
+	// анимация мобильного меню
+	$(".js-nav-ctrl").on('click', function(e) {
+	    e.preventDefault();
+	    var self = $(this),
+	        target = $('.' + self.data('nav'));
+
+	    self.toggleClass('is-open');
+	    target.toggleClass('is-open');
+	});
+});
+
+
+
+function goToElement () {
+
+	var animationAllow = 'is-animate';
+	var openState = 'is-open';
+	var nav = $('.js-nav');
+	var navControl = $('.js-nav__control');
+	var navPhone = $('.js-nav__phone');
+	var navH = 40;
+
+	$(".js-goto").click(function(e) {
+
+		nav.removeClass(animationAllow + ' ' + openState);
+		navControl.removeClass(animationAllow + ' ' + openState);
+		navPhone.removeClass(openState);
+		$('body').removeClass(openState);
+
+		var self = $(this),
+			targetClass = self.data('goto');
+		    target = $(targetClass);
+
+		var speed = 1000;
+
+		 e.preventDefault();
+
+
+	    $('html, body').stop().animate({
+	        scrollTop: target.offset().top - (navH + 10)
+	        
+	    }, speed, 'easeInOutCubic');
+	});
+
+} // goToElement
+
